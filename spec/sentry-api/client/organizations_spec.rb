761d101e13bdc799c271b3d27f151be6548720a0
面川泰明 <omokawa@senk-inc.co.jp>
2022-02-15 09:18:07 +0900

    done

--- spec/sentry-api/client/organizations_spec.rb
+++ spec/sentry-api/client/organizations_spec.rb
@@ -21,13 +21,12 @@ describe SentryApi::Client do
 
   describe ".organization_projects" do
     before do
-      stub_get("/organizations/sentry-sc/projects/", "organization_projects")
+      # stub_get("/organizations/sentry-sc/projects/", "organization_projects")
       @projects = SentryApi.organization_projects
     end
 
     it "should get the correct resource" do
       expect(a_get("/organizations/sentry-sc/projects/")).to have_been_made
-      binding.b
     end
 
     it "should return a paginated response of organization's projects" do
