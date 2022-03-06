b6e737145d695c4378827815ec3a788d96f7b0d0
面川泰明 <omokawa@senk-inc.co.jp>
2022-03-06 17:50:22 +0900

    done

--- memo.rb
+++ memo.rb
@@ -35,3 +35,26 @@ defined_check
 def m4
   get_hash.except(*EXCLUDES)
 end
+
+module Hoge
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def self.receiver_is_self
+    'receiver is self'
+  end
+
+  module ClassMethods
+    def c_method1
+      'class method'
+    end
+  end
+end
+
+class Fuga
+  include Hoge
+end
+
+p Hoge.receiver_is_self
+p Fuga.c_method1
