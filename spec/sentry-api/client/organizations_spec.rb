7cd2b75c714fe8ef26941a003bed9a1218496ee5
面川泰明 <omokawa@senk-inc.co.jp>
2022-03-08 12:39:04 +0900

    done

--- spec/sentry-api/client/organizations_spec.rb
+++ spec/sentry-api/client/organizations_spec.rb
@@ -27,6 +27,7 @@ describe SentryApi::Client do
     end
 
     it "should get the correct resource" do
+      binding.b
       expect(a_get("/organizations/sentry-sc/projects/")).to have_been_made
     end
 
