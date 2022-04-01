d3175027582e81e27aba7c2d18f9d86ea44639a7
面川泰明 <omokawa@senk-inc.co.jp>
2022-04-01 14:15:58 +0900

    done

--- memo.rb
+++ memo.rb
@@ -58,3 +58,21 @@ end
 
 p Hoge.receiver_is_self
 p Fuga.c_method1
+
+class Foo
+  def foo() "foo" end
+  def bar() "bar" end
+  def baz() "baz" end
+end
+
+# 任意のキーとメソッドの関係をハッシュに保持しておく
+# レシーバの情報がここにはないことに注意
+methods = {1 => Foo.instance_method(:foo),
+           2 => Foo.instance_method(:bar),
+           3 => Foo.instance_method(:baz)}
+
+# キーを使って関連するメソッドを呼び出す
+# レシーバは任意(Foo クラスのインスタンスでなければならない)
+p methods[1].bind(Foo.new).call      # => "foo"
+p methods[2].bind(Foo.new).call      # => "bar"
+p methods[3].bind(Foo.new).call      # => "baz"
